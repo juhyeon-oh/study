a4a6596fa58dede41a9a62bb6dfb7528c55a11dc
juhyeon-oh <juhyeon.oh@banksalad.com>
2024-07-24 17:37:15 +0900

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    kotlin("jvm") version "1.9.23"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testImplementation(kotlin("test"))
+}
+
+application {
+    mainClass.set("org.example.MainKt")
+    //applicationDefaultJvmArgs = listOf("-Dfile.encoding=UTF-8")
+}
+
+kotlin {
+    jvmToolchain(17)
+    sourceSets {
+        val main by getting {
+            kotlin.srcDirs("src/main/kotlin")
+        }
+    }
+}
+
+tasks.named<JavaExec>("run") {
+    standardInput = System.`in`
+}
